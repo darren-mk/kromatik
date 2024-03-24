5a5d0bbf6e53eb0ba5174e7eb0a919a7718ce210
Darren Kim <darren.minsoo.kim@gmail.com>
2024-03-24 18:56:45 -0400

    wrote source code and tests

--- src/kromatik/core.clj
+++ src/kromatik/core.clj
@@ -1,6 +1,30 @@
 (ns kromatik.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmulti ->ns-map
+  (fn [_ x] (coll? x)))
+
+(defn find-nsk [paths k]
+  (when-not (empty? paths)
+    (let [[path-nsk path-vals] (first paths)]
+      (if (contains? path-vals k)
+        path-nsk
+        (find-nsk (rest paths) k)))))
+
+(defmethod ->ns-map true
+  [m paths]
+  (let [fmt (fn [k]
+              (let [nsk (find-nsk paths k)]
+                (keyword (str (name nsk) "/" (name k)))))
+        comb (fn [acc [k v]] (assoc acc (fmt k) v))]
+    (reduce comb {} m)))
+
+(defmethod ->ns-map false
+  [m nsk]
+  (let [fmt (fn [k] (keyword (str (name nsk) "/" (name k))))
+        comb (fn [acc [k v]] (assoc acc (fmt k) v))]
+    (reduce comb {} m)))
+
+(defn ->bare-map [m]
+  (let [fmt (fn [k] (-> k name keyword))
+        comb (fn [acc [nsk v]] (assoc acc (fmt nsk) v))]
+    (reduce comb {} m)))
--- test/kromatik/core_test.clj
+++ test/kromatik/core_test.clj
@@ -1,7 +1,48 @@
 (ns kromatik.core-test
-  (:require [clojure.test :refer :all]
-            [kromatik.core :refer :all]))
+  (:require
+   [clojure.test :as t]
+   [kromatik.core :as src]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(t/deftest find-nsk-test
+  (t/testing "sought key exists in path vals"
+    (t/is
+     (= :abc
+        (src/find-nsk {:abc #{:a :b}
+                       :def #{:c :d}} :a))))
+  (t/testing "sought key not exists in path vals"
+    (t/is
+     (nil?
+      (src/find-nsk {:abc #{:a :b}
+                     :def #{:c :d}} :y)))))
+
+(t/deftest ->ns-map-test
+  (t/testing "single ns key is given"
+    (t/is
+     (= #:hello{:a 1, :b 2}
+        (src/->ns-map {:a 1 :b 2} :hello))))
+  (t/testing "correct detailed paths for ns key is given"
+    (t/is
+     (= {:hello/a 1, :bye/b 2}
+        (src/->ns-map
+         {:a 1 :b 2}
+         {:hello #{:a}
+          :bye #{:b}}))))
+  (t/testing "correct detailed but redundant paths is given"
+    (t/is
+     (= {:hello/a 1, :bye/b 2}
+        (src/->ns-map
+         {:a 1 :b 2}
+         {:hello #{:a :x :y :z}
+          :bye #{:b}})))))
+
+(t/deftest ->bare-map-test
+  (t/testing "all given keys are namespace"
+    (t/is
+     (= {:a 1 :b 2}
+        (src/->bare-map
+         {:xyz/a 1 :zyx/b 2}))))
+  (t/testing "some given keys are namespace"
+    (t/is
+     (= {:a 1 :b 2}
+        (src/->bare-map
+         {:xyz/a 1 :b 2})))))
