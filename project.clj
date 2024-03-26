eb1d5ee4166683f8808d903b0df37a8da9e2adcb
Darren Kim <darren.minsoo.kim@gmail.com>
2024-03-26 06:42:01 -0400

    pushed up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kromatik "0.0.1-SNAPSHOT"
+(defproject kromatik "0.0.2-SNAPSHOT"
   :description "Tiny toolset converting between namespaced and unnamespaced maps."
   :url "https://github.com/darren-m-kim/kromatik"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
